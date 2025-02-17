d41f1662e42c29b767c1b6650e92d8e39ad242df
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 08:26:07 +0000

    nested-dynamic-routing

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,7 @@
+export default function ClientProjectsPage() {
+    return (
+        <div>
+            <h1>The Client Project Page</h1>
+        </div>
+    );
+}
---
+++ pages/clients/index.js
@@ -0,0 +1,5 @@
+export default function ClientsPage() {
+    return (<div>
+        <h1>The Clients Page</h1>
+    </div>);
+}
