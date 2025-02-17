bb34b1d679a64ab5e0022a48c20ae3d3bd05f12a
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 08:40:11 +0000

    nested-links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+    const clients = [
+        { id : 'max', name: 'Maximillan'},
+        { id : 'vaish', name: 'Vaishali'},
+        { id : 'sharmi', name: 'Sharmila'},
+    ]
     return (<div>
         <h1>The Clients Page</h1>
+        <ul>
+            {clients.map(client => <li key={client.id}>
+                <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>)}
+        </ul>
     </div>);
 }
