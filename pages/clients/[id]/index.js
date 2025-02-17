afd6461dfd0128bf6dbd9c960ce883d15dfab6a6
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-17 08:46:08 +0000

    navigating-programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage() {
+    const router = useRouter();
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[projectid]',
+            query: {id: 'max', clientprojectid: 'A'}
+        });
+    }
     return (
         <div>
             <h1>The Client Project Page</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
